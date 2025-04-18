41c4322d008af75c5e9e1635e6dd3c98a7338c65
Michael Sanchez <michaelleesanchez1@gmail.com>
2025-04-18 13:34:17 -0700

    add under construction text

---
+++ app/PageLayout.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+const PageLayout = () => {
+ return (
+<div className="container">
+      <h2>DJ Dan 'TekkMeow' tour dates</h2>
+      <h4>coming soon...</h4>
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          font-family: 'Arial', sans-serif;
+          background-color: #222222;
+          color: #dddddd;
+          padding: 50px;
+        }
+      `}</style>
+    </div>
+ )
+}
+
+export default PageLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import PageLayout from "./PageLayout";
+
 export default function Home() {
   return (
-    <main>
-      <div>Next.js on GitHub Pages</div>
-    </main>
+    <PageLayout />
   );
 }
