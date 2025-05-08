2902bb8f630602dbd1c04df1edd7db12808a320b
Michael Sanchez <michaelleesanchez1@gmail.com>
2025-05-08 12:46:12 -0700

    add page footer for music and social links

--- app/styles/colors.js
+++ app/styles/colors.js
@@ -1,7 +1,20 @@
+const gradients = {
+  lightBlue: "179, 210, 230, 1",
+  lilac: "188, 179, 230, 1",
+  snuff: "232, 207, 236, 1",
+};
+
 export const colors = {
-    offWhite: '#dddddd',
-    dimWhite: '#bbbbbb',
-    charcoal: '#222222',
-    lightGrapefruit: 'hsl(15 50% 80%)',
-    clear: 'transparent',
-}
+  offWhite: "#dddddd",
+  dimWhite: "#bbbbbb",
+  charcoal: "#222222",
+  blackOpaque: "#00000020",
+  lightGrapefruit: "hsl(15 50% 80%)",
+  lightBlue: "#b3d2e6",
+  pink: "#e1b3e6",
+  pinkOpaque: "rgb(225 179 230 / 56%)",
+  snuff: "#e8cfec",
+  lilac: "#bcb3e6",
+  clear: "transparent",
+  buttonGradient: `linear-gradient(90deg,rgba(${gradients.lightBlue}) 0%, rgba(${gradients.lilac}) 70%, rgba(${gradients.lilac}) 100%)`,
+};
---
+++ data/externalLinks.js
@@ -0,0 +1,9 @@
+export const links = {
+  instereo: "https://linktr.ee/instereorecordings",
+  beatport: "https://www.beatport.com/artist/dj-dan/5416",
+  soundcloud: "https://soundcloud.com/djdanmusic",
+  spotify:
+    "https://open.spotify.com/artist/4w7PNHWFtN2CNkDNhDL7c3?si=COkFl7KdQxGBh7i5kfE-VQ",
+  instagram: "https://www.instagram.com/djdanmusic",
+  facebook: "https://www.facebook.com/DJDanMusic",
+};
